1d126b196432237682ed745bc65cc38525080b33
buckshot307 <buckshot307@gmail.com>
2020-11-11 22:37:22 -0500

    Renamed some files, copied stuff from tutorial, going to dig into it later and rewrite to fit my API.

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 require('dotenv').config({ path: './main.env' });
+require('./passport');
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const index = require('./routes/index');
 const auth = require('./routes/auth');
+const user = require('./routes/user');
 const mongoose = require('mongoose');
-const User = require('./models/user');
+const User = require('./models/users');
 
 var app = express();
 
@@ -21,23 +27,48 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', index);
+app.use('/user', passport.authenticate('jwt', {session: false}), user);
 app.use('/auth', auth);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({
+  credentials: true,
+}));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 
-passport.use(new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password'
-}, 
-function (email, password, cb) {
-  //this one is typically a DB call. Assume that the returned user object is pre-formatted and ready for storing in JWT
-  return User.findOne({email, password})
-     .then(user => {
-         if (!user) {
-             return cb(null, false, {message: 'Incorrect email or password.'});
-         }
-         return cb(null, user, {message: 'Logged In Successfully'});
-    })
-    .catch(err => cb(err));
-}
-));
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+module.exports = app;

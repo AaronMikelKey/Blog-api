cfffeba2815784195c73cdd7330fb3b0f21e328f
buckshot307 <buckshot307@gmail.com>
2020-11-17 18:04:10 -0500

    Fixed authentication from passport.js and auth route

--- passport.js
+++ passport.js
@@ -1,45 +1,52 @@
-const passport    = require('passport');
+const passport = require('passport');
 const passportJWT = require("passport-jwt");
 const ExtractJWT = passportJWT.ExtractJwt;
 const LocalStrategy = require('passport-local').Strategy;
-const JWTStrategy   = passportJWT.Strategy;
+const JWTStrategy = passportJWT.Strategy;
+const bcrypt = require('bcryptjs');
+const User = require('./models/users');
 
 passport.use(new LocalStrategy({
-        usernameField: 'email',
-        passwordField: 'password'
-    },
-    function (email, password, cb) {
+  usernameField: 'email',
+  passwordField: 'password'
+},
 
-        //Assume there is a DB module pproviding a global UserModel
-        return UserModel.findOne({email, password})
-            .then(user => {
-                if (!user) {
-                    return cb(null, false, {message: 'Incorrect email or password.'});
-                }
-
-                return cb(null, user, {
-                    message: 'Logged In Successfully'
-                });
-            })
-            .catch(err => {
-                return cb(err);
+  (email, password, cb) => {
+    //Assume there is a DB module pproviding a global UserModel
+    return User.findOne({ email })
+      .then(user => {
+        if (!user) {
+          return cb(null, false, { message: 'Incorrect email.' });
+        }
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            return cb(null, user, {
+              message: 'Logged In Successfully'
             });
-    }
+          } else {
+            return cb(null, false, { msg: 'Incorrect password.' })
+          }
+        })
+      })
+      .catch(err => {
+        return cb(err);
+      });
+  }
 ));
 
 passport.use(new JWTStrategy({
-        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-        secretOrKey   : 'your_jwt_secret'
-    },
-    function (jwtPayload, cb) {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'your_jwt_secret'
+},
+  function (jwtPayload, cb) {
 
-        //find the user in db if needed
-        return UserModel.findOneById(jwtPayload.id)
-            .then(user => {
-                return cb(null, user);
-            })
-            .catch(err => {
-                return cb(err);
-            });
-    }
+    //find the user in db if needed
+    return User.findOneById(jwtPayload.id)
+      .then(user => {
+        return cb(null, user);
+      })
+      .catch(err => {
+        return cb(err);
+      });
+  }
 ));
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 
+const bcrypt = require('bcryptjs');
 const jwt      = require('jsonwebtoken');
 const passport = require('passport');
 
@@ -21,15 +22,18 @@ router.post('/login', function (req, res, next) {
             if (err) {
                 res.send(err);
             }
+            const body = {
+              _id: user._id,
+              username: user.username
+            }
 
-            const token = jwt.sign(user, process.env.JWT_Token);
+            const token = jwt.sign({user: body}, process.env.JWT_Token);
             res.cookie('auth', token, 
               { 
-                domain: '.example.com',
                 path: '/',
                 secure: true,
                 httpOnly: true,
-                sameSite: true
+                sameSite: "none"
                 })
             return res.json({user, token});
         });

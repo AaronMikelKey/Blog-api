8aba6b3f266e633d41fd55255e442af81e6bfb2a
buckshot307 <buckshot307@gmail.com>
2020-11-26 15:06:33 -0500

    Added app.use(bodyParser(raw)) to try and get json stuff

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(bodyParser.raw());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());

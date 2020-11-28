e47b9266ea2521db1c23bc36b6923aa9f3d51669
buckshot307 <buckshot307@gmail.com>
2020-11-27 20:15:56 -0500

    added localhost:8080 as origin for testing

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
+  origin: 'localhost:8080',
   credentials: true,
 }));
 

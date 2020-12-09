fd04acfe533d53d05fd35276475960650b327e2c
buckshot307 <buckshot307@gmail.com>
2020-12-09 17:00:41 -0500

    Fixed mistake on comment POST route

--- routes/comment.js
+++ routes/comment.js
@@ -9,7 +9,7 @@ router.get('/:commentId', (req, res) => {
 })
 
 //POST request for new comment
-router.post('/:commentId', commentController.commentPost)
+router.post('/', commentController.commentPost)
 
 //PUT request for comment
 router.put('/:commentId', commentController.commentUpdate)

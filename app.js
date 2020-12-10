d9e1357f46df89c70b148a8171205a77c8bb65d4
buckshot307 <buckshot307@gmail.com>
2020-12-09 19:01:22 -0500

    Possible bug fix to CORS error

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
   origin: 'http://localhost:8080',
+  vary: 'Origin',
   credentials: true,
   methods: 'POST,PUT,GET,DELETE,OPTIONS'
 }));

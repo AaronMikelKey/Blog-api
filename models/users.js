1bf13eb3f2c8da873a57b8c7ec7a3f813f3d12b7
buckshot307 <buckshot307@gmail.com>
2020-12-10 18:03:01 -0500

    Modified user model
    Changed user model by adding static findOneOrCreate as used by passport-facebook strategy.  Might have to redo UserSchema completely to enusre ease of auth with facebook

--- models/users.js
+++ models/users.js
@@ -13,19 +13,40 @@ const UserSchema = new Schema({
 
 UserSchema.pre('save', function (next) {
   var user = this;
-  if (!user.isModified('password')) {return next()};
-  bcrypt.hash(user.password,10).then((hashedPassword) => {
-      user.password = hashedPassword;
-      next();
+  if (!user.isModified('password')) { return next() };
+  bcrypt.hash(user.password, 10).then((hashedPassword) => {
+    user.password = hashedPassword;
+    next();
   })
 }, function (err) {
   next(err)
 })
-UserSchema.methods.comparePassword=function(candidatePassword,next){    
-    bcrypt.compare(candidatePassword,this.password,function(err,isMatch){
-      if(err) return next(err);
-      next(null,isMatch)
+UserSchema.methods.comparePassword = function (candidatePassword, next) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return next(err);
+    next(null, isMatch)
   })
 }
+
+UserSchema.statics.findOneOrCreate = function findOneOrCreate(condition, doc) {
+  const self = this;
+  const newDocument = doc;
+  return new Promise((resolve, reject) => {
+    return self.findOne(condition)
+      .then((result) => {
+        if (result) {
+          return resolve(result);
+        }
+        return self.create(newDocument)
+          .then((result) => {
+            return resolve(result);
+          }).catch((error) => {
+            return reject(error);
+          })
+      }).catch((error) => {
+        return reject(error);
+      })
+  });
+};
 
 module.exports = mongoose.model('User', UserSchema)

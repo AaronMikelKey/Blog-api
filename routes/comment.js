5306426e09dc1c8f397de51658d410dbd8a0f4da
buckshot307 <buckshot307@gmail.com>
2020-12-09 19:27:39 -0500

    Possible cors bug fix

--- routes/comment.js
+++ routes/comment.js
@@ -9,7 +9,12 @@ router.get('/:commentId', (req, res) => {
 })
 
 //POST request for new comment
-router.post('/newComment', commentController.commentPost)
+router.post('/newComment',{headers: {
+  'Access-Control-Allow-Headers':'Content-Type',
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': 'http://localhost:8080',
+  'Vary':'Origin'
+}}, commentController.commentPost)
 
 //PUT request for comment
 router.put('/:commentId', commentController.commentUpdate)
